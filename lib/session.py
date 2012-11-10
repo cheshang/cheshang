2890f23bfdd221ef9e9b6f5a1122c503b1b3b3b2
Lerry <lvdachao@gmail.com>
2012-11-10 13:55:51 +0800

    f

--- lib/session.py
+++ lib/session.py
@@ -21,6 +21,12 @@ class Session(object):
         else:
             db.session.remove({'_id':uid})
 
+    @staticmethod
+    def id_by_value(session):
+        if session:
+            result = db.session.find_one({'session':session})
+            return str(result['_id']) if result else None
+
 
 # 接口
 ############################################
@@ -29,47 +35,13 @@ def session_new(id):
     if not s:
         s = str(uuid.uuid4()).replace('-', '')
         Session.set(id, s)
-    return _id_binary_encode(id, s)
+    return s
 
 def id_by_session(session):
-    if session:
-        id, binary = _id_binary_decode(session)
-        if id and binary and binary == Session.get(id):
-            return id
+    return Session.id_by_value(session)
 
 def session_rm(id):
     Session.set(id, None)
-
-
-# 内部函数
-############################################
-
-def _id_by_base64(string):
-    try:
-        id = urlsafe_b64decode(string+'==')
-    except (binascii.Error, TypeError):
-        return 0
-    else:
-        return unpack('Q', id)[0]
-
-def _id_binary_decode(session):
-    if not session:
-        return None, None
-    id = session[:11]
-    value = session[11:]
-    try:
-        value = urlsafe_b64decode(value+'=')
-    except (binascii.Error, TypeError):
-        return None, None
-    id = _id_by_base64(id)
-    return id, value
-
-def _id_binary_encode(id, session):
-    id_key = pack('Q', id)
-    id_key = urlsafe_b64encode(id_key).rstrip('=')
-    ck_key = urlsafe_b64encode(session)
-    return '%s%s'%(id_key, ck_key)
-
 
 if __name__ == '__main__':
     print session_new(12345)
