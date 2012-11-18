7bf30b4bcf74ab729209d6c7a97a5e0adf364b2d
Lerry <lvdachao@gmail.com>
2012-11-18 23:50:55 +0800

    fix

--- config/default.py
+++ config/default.py
@@ -14,12 +14,10 @@ render = render_jinja(
 HOST = "lerry.me"
 
 MYSQL_HOST = '127.0.0.1'
-if 'Linux' not in platform.platform():
-    MYSQL_HOST = '113.11.199.27'
 MYSQL_PORT = 3306
 MYSQL_USER = 'work'
 MYSQL_PASSWD = '42qu'
 MYSQL_DB = 'work_car'
 
-WEIBO_KEY  = '2640561235'
-WEIBO_SECRET = 'e9291642a66f70a3d8db5fb6b8c60949'
+WEIBO_KEY  = '123'
+WEIBO_SECRET = 'xxxooo'
