861e15bcbb70fdb9918155c830ae1ba7316e2fcd
Lerry <lvdachao@gmail.com>
2012-11-19 00:43:47 +0800

    fix

--- model/upyun.py
+++ model/upyun.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import _env
 import hashlib
 from json import dumps
 from base64 import b64encode
-from misc.config import UPYUN
+from config import UPYUN
 
 def policy_signature_get(bucket):
-key , host, bucket = ('123', '456', 'avatar')
-policy = {
-    'bucket'        : bucket,
-    'expiration'    : int(time()) + 360000,
-    'save-key'      : '/{filemd5}',
-}
-policy = b64encode(dumps(policy))
-signature = hashlib.md5('%s&%s'% (policy, key)).hexdigest()
-%>
+    key, host = UPYUN[bucket]
+    policy = {
+        'bucket'        : bucket,
+        'expiration'    : int(time()) + 360000,
+        'save-key'      : '/{filemd5}',
+    }
+    policy = b64encode(dumps(policy))
+    signature = hashlib.md5('%s&%s'% (policy, key)).hexdigest()
+    return policy, signature
