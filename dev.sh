30288992848da5e6128911bf725fb598fa86b7ee
Lerry <lvdachao@gmail.com>
2012-11-28 23:26:46 +0800

    fix

--- dev.sh
+++ dev.sh
@@ -1,1 +1,9 @@
-python app.py 8080
+PREFIX=$(cd "$(dirname "$0")"; pwd)
+
+#python $PREFIX/misc/boot/css_js.py
+PROGRAM=$PREFIX/app.py
+ps x -u $USER|ack $PROGRAM|awk  '{print $1}'|xargs kill -9
+ps x -u $USER|ack 'jitter'|awk  '{print $1}'|xargs kill -9
+jitter $PREFIX/coffee $PREFIX/static/js &
+
+python $PROGRAM
