50b07a401c8acecfa2045b8c5e6f8fc58fce6ee5
Lerry <lvdachao@gmail.com>
2012-12-05 23:56:03 +0800

    fix

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,5 @@
 function LOAD(js){ document.write('<script src="'+js+'"></'+"script>") }
 LOAD('http://lib.sinaapp.com/js/jquery/1.8.3/jquery.min.js')
 LOAD('/static/js/lib/post_json.js')
+LOAD('/static/js/lib/happy.js')
+LOAD('/static/js/lib/happy.methods.js')
