50526b1aeb4aa73040807bf01df07cb5ccc579ab
Lerry <lvdachao@gmail.com>
2013-01-03 17:15:03 +0800

    fix

--- view/album.py
+++ view/album.py
@@ -6,31 +6,34 @@ from view._base import route, View, LoginView
 from model.album import album_new, album_get
 from model.photo import photo_new
 
-@route('/album/new')
+
 class New(LoginView):
     def GET(self):
-        return self.render()
+        return self.render(template_name='album/upload.html')
 
     def POST(self):
         name, txt = self.arguments('name', 'txt')
         if name:
-            album = album_new(name, txt, self.uid)
+            
             self.redirect('/album/%s' % album.id)
         self.render()
 
-@route('/album/(\d+)/upload')
+@route('/album/new')
 class Upload(LoginView):
-    def GET(self, album_id=0):
-        album = album_get(album_id)
-        if not album or not album.can_edit(self.uid):
-            self.redirect('/album/%s' % album_id)
+    def GET(self):
         return self.render()
 
-    def POST(self, album_id=0):
-        album = album_get(album_id)
+    def POST(self):
+        #return web.input(url=[], name=[], size=[])
+        album_id, title, txt = self.arguments('album_id', 'title', 'txt')
+        album_id = int(album_id or 0)
+        if not album_id and title:
+            album = album_new(title, txt, self.uid)
+        else:
+            album = album_get(album_id)
         if not album or not album.can_edit(self.uid):
-            self.redirect('/album/%s' % album_id)
-        uid = self.current_user_id
+            return self.redirect('/album/%s' % album_id)
+        uid = self.uid
         data = web.input(url=[], name=[], size=[])
         urls, sizes, names = [data[i] for i in ('url', 'size', 'name')]
         for url, size, name  in zip(urls, sizes, names):
